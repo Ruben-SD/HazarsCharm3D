9c8e3aca84cf854adf80805e8a168e2d8fb708d6
Blanca <blanca.fernandez.martin@udc.es>
2021-12-18 23:09:54 +0100

    everything works but avatar change is not made

---
+++ Assets/ChangeAvatar.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class ChangeAvatar : MonoBehaviour
+{
+    public string tagName;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        GameObject rocket, player;
+        if (col.tag == tagName)
+        {
+            player = col.gameObject.transform.parent.gameObject;
+            SetChildren(player, false);
+
+            rocket = this.transform.parent.gameObject.transform.parent.gameObject;
+            EnableScripts(rocket);
+            EnableComponents(rocket);
+            SetChildren(rocket, true);
+        }
+    }
+
+    private void EnableScripts(GameObject gameObject)
+    {
+        MonoBehaviour[] scriptComponents = gameObject.GetComponents<MonoBehaviour>();
+        foreach (MonoBehaviour script in scriptComponents)
+        {
+            script.enabled = true;
+        }
+    }
+
+    private void EnableComponents(GameObject gameObject)
+    {
+        gameObject.GetComponent<Animator>().enabled = true;
+        gameObject.GetComponent<CharacterController>().enabled = true;
+    }
+
+    private void SetChildren(GameObject gameObject, bool option)
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(option);
+        }
+    }
+}
